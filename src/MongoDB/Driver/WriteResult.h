0bbcfa0014fa296b1df088a3b7e04861331cfea7
Vincent Lechemin <vincent.lechemin@gmail.com>
2018-09-02 13:22:29 +0200

    Added destructor to fix memory leak

--- src/MongoDB/Driver/WriteResult.h
+++ src/MongoDB/Driver/WriteResult.h
@@ -34,7 +34,7 @@ class MongoDBDriverWriteResultData
 		static Class* getClass();
 
 		void sweep() {
-			/* do nothing */
+			mongoc_write_concern_destroy(m_write_concern);
 		}
 
 		~MongoDBDriverWriteResultData() {
